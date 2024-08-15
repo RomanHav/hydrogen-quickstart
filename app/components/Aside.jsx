f074215db091399b57dd7ce2745da50dcfc104c5
RomanHav <roman14082004@gmail.com>
2024-08-15 21:47:28 +0300

    Scaffold Storefront

---
+++ app/components/Aside.jsx
@@ -0,0 +1,77 @@
+import {createContext, useContext, useState} from 'react';
+
+/**
+ * A side bar component with Overlay
+ * @example
+ * ```jsx
+ * <Aside type="search" heading="SEARCH">
+ *  <input type="search" />
+ *  ...
+ * </Aside>
+ * ```
+ * @param {{
+ *   children?: React.ReactNode;
+ *   type: AsideType;
+ *   heading: React.ReactNode;
+ * }}
+ */
+export function Aside({children, heading, type}) {
+  const {type: activeType, close} = useAside();
+  const expanded = type === activeType;
+
+  return (
+    <div
+      aria-modal
+      className={`overlay ${expanded ? 'expanded' : ''}`}
+      role="dialog"
+    >
+      <button className="close-outside" onClick={close} />
+      <aside>
+        <header>
+          <h3>{heading}</h3>
+          <button className="close reset" onClick={close}>
+            &times;
+          </button>
+        </header>
+        <main>{children}</main>
+      </aside>
+    </div>
+  );
+}
+
+const AsideContext = createContext(null);
+
+Aside.Provider = function AsideProvider({children}) {
+  const [type, setType] = useState('closed');
+
+  return (
+    <AsideContext.Provider
+      value={{
+        type,
+        open: setType,
+        close: () => setType('closed'),
+      }}
+    >
+      {children}
+    </AsideContext.Provider>
+  );
+};
+
+export function useAside() {
+  const aside = useContext(AsideContext);
+  if (!aside) {
+    throw new Error('useAside must be used within an AsideProvider');
+  }
+  return aside;
+}
+
+/** @typedef {'search' | 'cart' | 'mobile' | 'closed'} AsideType */
+/**
+ * @typedef {{
+ *   type: AsideType;
+ *   open: (mode: AsideType) => void;
+ *   close: () => void;
+ * }} AsideContextValue
+ */
+
+/** @typedef {import('react').ReactNode} ReactNode */
---
+++ app/components/CartSummary.jsx
@@ -0,0 +1,112 @@
+import {CartForm, Money} from '@shopify/hydrogen';
+
+/**
+ * @param {CartSummaryProps}
+ */
+export function CartSummary({cart, layout}) {
+  const className =
+    layout === 'page' ? 'cart-summary-page' : 'cart-summary-aside';
+
+  return (
+    <div aria-labelledby="cart-summary" className={className}>
+      <h4>Totals</h4>
+      <dl className="cart-subtotal">
+        <dt>Subtotal</dt>
+        <dd>
+          {cart.cost?.subtotalAmount?.amount ? (
+            <Money data={cart.cost?.subtotalAmount} />
+          ) : (
+            '-'
+          )}
+        </dd>
+      </dl>
+      <CartDiscounts discountCodes={cart.discountCodes} />
+      <CartCheckoutActions checkoutUrl={cart.checkoutUrl} />
+    </div>
+  );
+}
+/**
+ * @param {{checkoutUrl?: string}}
+ */
+function CartCheckoutActions({checkoutUrl}) {
+  if (!checkoutUrl) return null;
+
+  return (
+    <div>
+      <a href={checkoutUrl} target="_self">
+        <p>Continue to Checkout &rarr;</p>
+      </a>
+      <br />
+    </div>
+  );
+}
+
+/**
+ * @param {{
+ *   discountCodes?: CartApiQueryFragment['discountCodes'];
+ * }}
+ */
+function CartDiscounts({discountCodes}) {
+  const codes =
+    discountCodes
+      ?.filter((discount) => discount.applicable)
+      ?.map(({code}) => code) || [];
+
+  return (
+    <div>
+      {/* Have existing discount, display it with a remove option */}
+      <dl hidden={!codes.length}>
+        <div>
+          <dt>Discount(s)</dt>
+          <UpdateDiscountForm>
+            <div className="cart-discount">
+              <code>{codes?.join(', ')}</code>
+              &nbsp;
+              <button>Remove</button>
+            </div>
+          </UpdateDiscountForm>
+        </div>
+      </dl>
+
+      {/* Show an input to apply a discount */}
+      <UpdateDiscountForm discountCodes={codes}>
+        <div>
+          <input type="text" name="discountCode" placeholder="Discount code" />
+          &nbsp;
+          <button type="submit">Apply</button>
+        </div>
+      </UpdateDiscountForm>
+    </div>
+  );
+}
+
+/**
+ * @param {{
+ *   discountCodes?: string[];
+ *   children: React.ReactNode;
+ * }}
+ */
+function UpdateDiscountForm({discountCodes, children}) {
+  return (
+    <CartForm
+      route="/cart"
+      action={CartForm.ACTIONS.DiscountCodesUpdate}
+      inputs={{
+        discountCodes: discountCodes || [],
+      }}
+    >
+      {children}
+    </CartForm>
+  );
+}
+
+/**
+ * @typedef {{
+ *   cart: OptimisticCart<CartApiQueryFragment | null>;
+ *   layout: CartLayout;
+ * }} CartSummaryProps
+ */
+
+/** @typedef {import('storefrontapi.generated').CartApiQueryFragment} CartApiQueryFragment */
+/** @typedef {import('~/components/CartMain').CartLayout} CartLayout */
+/** @typedef {import('@shopify/hydrogen').OptimisticCart} OptimisticCart */
